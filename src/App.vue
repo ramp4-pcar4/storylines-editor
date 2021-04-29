42562ead5e9e1c63adeb07a076b64ac892d57a6b
Aleksuei Riabtsev <2285779+AleksueiR@users.noreply.github.com>
2021-04-29 12:45:53 -0400

    chore: renaming things

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
     <div id="app">
         <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-        <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
-        <Steps />
+        <Story />
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-import Steps from '@/components/steps.vue';
+import Story from '@/components/story.vue';
 
 @Component({
     components: {
-        HelloWorld,
-        Steps
+        Story
     }
 })
 export default class App extends Vue {}
--- src/components/chapter.vue
+++ src/components/chapter.vue
@@ -1,6 +1,6 @@
 <template>
     <Scrollama
-        :id="`substeps-${stepIndex}`"
+        :id="`chapter-${stepIndex}`"
         @step-enter="({ element }) => (currStepTitle = element.dataset.stepTitle)"
         @step-exit="currStepTitle = null"
     >
@@ -30,49 +30,52 @@
             :data-step-title="step.title"
             :class="{ 'is-active': currStepTitle == step.title }"
         >
-            <h2>{{ stepTitle }} -- {{ step.title }}</h2>
+            <h3><!-- {{ stepTitle }} --  -->{{ step.title }}</h3>
             <div v-html="step.text"></div>
         </div>
     </Scrollama>
 </template>
 
-<script>
+<script lang="ts">
 import 'intersection-observer';
 import Scrollama from 'vue-scrollama';
 
-export default {
-    props: ['stepTitle', 'stepIndex', 'stepImage'],
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({
     components: {
         Scrollama
-    },
-    data() {
-        return {
-            currStepTitle: null,
-            subSteps: [
-                {
-                    title: 'A',
-                    text: `
+    }
+})
+export default class Chapter extends Vue {
+    @Prop() stepTitle!: string;
+    @Prop() stepIndex!: number;
+    @Prop() stepImage!: string;
+
+    currStepTitle = null;
+    subSteps = [
+        {
+            title: 'A',
+            text: `
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
                         <p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. </p>
                         `
-                },
-                {
-                    title: 'B',
-                    text: `
+        },
+        {
+            title: 'B',
+            text: `
                         <p>Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. </p>
                         `
-                },
-                {
-                    title: 'C',
-                    text: `
+        },
+        {
+            title: 'C',
+            text: `
                         <p>Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. </p>
                         <p>Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. </p>
                         `
-                }
-            ]
-        };
-    }
-};
+        }
+    ];
+}
 </script>
 
 <style src="vue-scrollama/dist/vue-scrollama.css"></style>
