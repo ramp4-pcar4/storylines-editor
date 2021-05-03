3139da258e3de5493c1a0db55b50ea5838d68e1c
Aleksuei Riabtsev <2285779+AleksueiR@users.noreply.github.com>
2021-05-03 14:22:33 -0400

    chore: add 8xl width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
     purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {}
+        extend: {},
+        maxWidth: {
+            '8xl': '90rem'
+        }
     },
     variants: {
         extend: {}
