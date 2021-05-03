f4aa16b72eeeda2b578c8ae9f640a4f0f562066f
Aleksuei Riabtsev <2285779+AleksueiR@users.noreply.github.com>
2021-05-03 10:10:12 -0400

    chore: use auto line endings in eslint

--- src/story-config.ts
+++ src/story-config.ts
@@ -37,7 +37,7 @@ const story: StoryConfig = {
     title: 'Story RAMP',
     chapters: [
         {
-            title: 'Spep 1',
+            title: 'Step 1',
             graphic: {
                 type: GraphicKind.Image,
                 payload: {
@@ -49,7 +49,7 @@ const story: StoryConfig = {
             },
             scenes: [
                 {
-                    title: 'Scene A', // optional
+                    title: 'Scene A',
                     text: `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero.
 
@@ -73,6 +73,27 @@ Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque vol
                     text: `
 Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris.
 Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet.
+`
+                }
+            ]
+        },
+        {
+            title: 'Step 2',
+            graphic: {
+                type: GraphicKind.Image,
+                payload: {
+                    src: 'https://cli.vuejs.org/favicon.png'
+                }
+            },
+            scenes: [
+                {
+                    title: 'Scene A',
+                    text: `
+                    
+First Term - This is the definition of the first term.
+
+Second Term - This is one definition of the second term.
+
 `
                 }
             ]
