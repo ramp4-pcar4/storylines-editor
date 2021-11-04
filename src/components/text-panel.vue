07c35d27a3bb0e95b51bbf9d3d7b44300f3bf0fe
Ryan Coulson <contact@ryan-coulson.com>
2021-11-04 13:36:17 -0400

    create basic slide and panel components

---
+++ src/components/text-panel.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="font-bold block text-lg">{{ config.title }}</div>
+        <div class="block">{{ config.content }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component({})
+export default class TextPanelV extends Vue {
+    @Prop() config!: any; // TODO: replace with TypeScript type
+}
+</script>
