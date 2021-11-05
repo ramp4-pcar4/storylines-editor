e7167e720d7656711c7c350c0bc3a8268133feaf
Yi Lei Feng <fengyilei98@hotmail.com>
2021-11-05 11:23:16 -0400

    implement text panels to render md content

---
+++ src/components/panels/text-panel.vue
@@ -0,0 +1,35 @@
+<template>
+    <Scrollama class="flex-1 prose max-w-none">
+        <h2 class="px-10 mb-0 chapter-title top-20">
+            {{ config.title }}
+        </h2>
+
+        <div class="px-10 md-content" v-html="md.render(config.content)"></div>
+    </Scrollama>
+</template>
+
+<script lang="ts">
+import Scrollama from 'vue-scrollama';
+import MarkdownIt from 'markdown-it';
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+interface TextPanelConfig {
+    title: string;
+    type: string;
+    content: string; // in md format
+}
+
+@Component({
+    components: {
+        Scrollama
+    }
+})
+export default class TextPanelV extends Vue {
+    @Prop() config!: TextPanelConfig;
+
+    md = new MarkdownIt();
+}
+</script>
+
+<style scoped lang="scss"></style>
