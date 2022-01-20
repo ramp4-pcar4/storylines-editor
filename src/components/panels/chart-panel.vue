8251dd28994690162f62f0ae29dfd8700a5ca8ef
Yi Lei Feng <fengyilei98@hotmail.com>
2022-01-20 15:10:37 -0500

    add support for basic pie charts

--- src/components/panels/chart-panel.vue
+++ src/components/panels/chart-panel.vue
@@ -54,54 +54,114 @@ export default class ChartPanelV extends Vue {
 
             // download: true needed for local files which is treated as an URL
             this.$papa.parse(data.url, {
-                header: true,
+                header: dqvOptions?.type === 'pie' ? false : true,
                 dynamicTyping: true,
                 download: true,
                 complete: (res: any) => {
-                    // process parsed CSV data (get fields and xaxis categories first)
-                    const fields = res.meta.fields;
-                    const cato = res.meta.fields.shift();
-                    const xAxis = {
-                        title: {
-                            text: dqvOptions?.xAxisLabel ? dqvOptions?.xAxisLabel : ''
-                        },
-                        categories: res.data.map((row: any[]) => row[cato])
-                    };
-
-                    // get all series data for each field
-                    let series: SeriesData[] = [];
-                    fields.forEach((f: string) => {
-                        const colData = res.data.map((row: any) => row[f]);
-                        // default to line graph (not sure best way to configure this with CSV input)
-                        series.push({
-                            name: f,
-                            data: colData,
-                            type: dqvOptions?.type ? dqvOptions?.type : 'line'
-                        });
-                    });
-
-                    // initializing chartOptions with filtered CSV data + configurable options (TODO)
-                    this.chartOptions = {
-                        series: series,
-                        xAxis: xAxis,
-                        title: {
-                            text: dqvOptions?.title ? dqvOptions?.title : ''
-                        },
-                        subtitle: {
-                            text: dqvOptions?.subtitle ? dqvOptions?.subtitle : ''
-                        },
-                        credits: {
-                            enabled: dqvOptions?.credits ? dqvOptions?.credits : false
-                        },
-                        yAxis: {
-                            title: {
-                                text: dqvOptions?.yAxisLabel ? dqvOptions?.yAxisLabel : ''
-                            }
-                        }
-                    };
+                    // construct highcharts objects based on chart type
+                    if (dqvOptions?.type === 'pie') {
+                        this.makePieChart(res.data);
+                    } else {
+                        this.makeLineChart(res.meta.fields, res.data);
+                    }
                 }
             });
         });
+    }
+
+    /**
+     * Parse chart data content and return a highcharts formatted series object for a pie chart.
+     */
+    makePieChart(data: any): void {
+        const dqvOptions = this.config.options;
+        let series: { data: SeriesData[] } = { data: [] };
+
+        // construct series data
+        data.forEach((slice: any) => {
+            series.data.push({
+                name: slice[0],
+                // in case of strings being passed in such as '10%'
+                y: parseFloat(slice[1])
+            });
+        });
+
+        // plot options for pie charts
+        const plotOptions = {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        };
+
+        // initializing chartOptions for line/bar charts
+        this.chartOptions = {
+            chart: {
+                type: dqvOptions?.type ? dqvOptions?.type : 'chart'
+            },
+            title: {
+                text: dqvOptions?.title ? dqvOptions?.title : ''
+            },
+            subtitle: {
+                text: dqvOptions?.subtitle ? dqvOptions?.subtitle : ''
+            },
+            plotOptions: plotOptions,
+            series: series,
+            credits: {
+                enabled: dqvOptions?.credits ? dqvOptions?.credits : false
+            }
+        };
+    }
+
+    /**
+     * Parse chart data content and return a highcharts formatted series object for a line/bar chart.
+     */
+    makeLineChart(fields: string[], data: any): void {
+        const dqvOptions = this.config.options;
+        // find xAxis categories for line/bar charts
+        const cato = fields.shift() as string;
+        const xAxis = {
+            title: {
+                text: dqvOptions?.xAxisLabel ? dqvOptions?.xAxisLabel : ''
+            },
+            categories: data.map((row: any) => row[cato])
+        };
+
+        // construct series data
+        let series: SeriesData[] = [];
+        fields.forEach((f: string) => {
+            const colData = data.map((row: any) => row[f]);
+            series.push({
+                name: f,
+                data: colData
+            });
+        });
+
+        // initializing chartOptions for line/bar charts
+        this.chartOptions = {
+            chart: {
+                type: dqvOptions?.type ? dqvOptions?.type : 'line'
+            },
+            series: series,
+            xAxis: xAxis,
+            title: {
+                text: dqvOptions?.title ? dqvOptions?.title : ''
+            },
+            subtitle: {
+                text: dqvOptions?.subtitle ? dqvOptions?.subtitle : ''
+            },
+            credits: {
+                enabled: dqvOptions?.credits ? dqvOptions?.credits : false
+            },
+            yAxis: {
+                title: {
+                    text: dqvOptions?.yAxisLabel ? dqvOptions?.yAxisLabel : ''
+                }
+            }
+        };
     }
 }
 </script>
