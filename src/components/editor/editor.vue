906b0c63053ad918d467ee03e19a8fe2a2302165
Rory Hofland <roryhofland@gmail.com>
2023-03-30 11:20:20 -0400

    duplicate uuid check

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -17,10 +17,33 @@
                     <input
                         type="text"
                         @input="error = false"
+                        v-on:blur="checkUuid"
                         v-model="uuid"
                         class="w-1/3"
                         :class="error ? 'input-error' : ''"
                     />
+                    <span v-if="warning" class="text-yellow-500 rounded p-1 ml-2">
+                        <span class="align-middle inline-block mr-1 pb-1 fill-current">
+                            <svg
+                                clip-rule="evenodd"
+                                fill-rule="evenodd"
+                                stroke-linejoin="round"
+                                stroke-miterlimit="2"
+                                viewBox="0 0 24 24"
+                                width="18"
+                                height="18"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="m2.095 19.886 9.248-16.5c.133-.237.384-.384.657-.384.272 0 .524.147.656.384l9.248 16.5c.064.115.096.241.096.367 0 .385-.309.749-.752.749h-18.496c-.44 0-.752-.36-.752-.749 0-.126.031-.252.095-.367zm9.907-6.881c-.414 0-.75.336-.75.75v3.5c0 .414.336.75.75.75s.75-.336.75-.75v-3.5c0-.414-.336-.75-.75-.75zm-.002-3c-.552 0-1 .448-1 1s.448 1 1 1 1-.448 1-1-.448-1-1-1z"
+                                    fill-rule="nonzero"
+                                />
+                            </svg>
+                        </span>
+                        <span class="align-center inline-block select-none"
+                            >UUID already exists. Saving this will overwrite existing product.</span
+                        >
+                    </span>
                     <button
                         @click="generateRemoteConfig"
                         class="bg-black text-white hover:bg-gray-800"
@@ -241,6 +264,7 @@ export default class EditorV extends Vue {
     configFileStructure: any = undefined;
     loadStatus = 'waiting';
     error = false; // whether an error has occurred
+    warning = false; // used for duplicate uuid warning
     lang = 'en';
     unsavedChanges = false;
     saving = false;
@@ -668,6 +692,17 @@ export default class EditorV extends Vue {
     updateSlides(slides: Slide[]): void {
         this.slides = slides;
         this.slideIndex = this.slides.indexOf(this.currentSlide);
+    }
+
+    checkUuid(): void {
+        if (!this.editExisting) {
+            fetch(`http://localhost:6040/retrieve/${this.uuid}`).then((res: any) => {
+                if (res.status !== 404) {
+                    this.warning = true;
+                }
+            });
+        }
+        this.warning = false;
     }
 
     /**
