04f7c9f55f9c885652cef47ab42727171dfee654
Ryan Coulson <ryan.coulson@ec.gc.ca>
2023-06-06 13:18:27 -0400

    fix panel contents covering header bar

--- src/components/panels/panel.vue
+++ src/components/panels/panel.vue
@@ -2,7 +2,7 @@
     <div
         :class="
             config.type !== 'text'
-                ? `sticky ${config.type === 'map' ? 'top-16' : 'top-8'} sm:self-start flex-2 order-1 sm:order-2 z-50`
+                ? `sticky ${config.type === 'map' ? 'top-16' : 'top-8'} sm:self-start flex-2 order-1 sm:order-2 z-40`
                 : 'flex order-2 sm:order-1'
         "
         class="flex-col relative"
