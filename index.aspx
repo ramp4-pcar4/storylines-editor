c716b8abb2f1e65008228688e080cf2f5e69de7e
Miles Petrov <miles.petrov@ec.gc.ca>
2023-12-06 16:09:13 -0500

    Setup new .NET application with the editor embedded

---
+++ index.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="" Language="C#"
+MasterPageFile="~/GoC.WebTemplate/GoCWebTemplate.Master" AutoEventWireup="true"
+CodeBehind="index.aspx.cs" Inherits="Storylines.base_page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+  <h1
+    class="pageTitle"
+    data-lang="<%=WebTemplateMaster.WebTemplateModel.TwoLetterCultureLanguage %>"
+    id="wb-cont"
+  >
+    <%=WebTemplateMaster.WebTemplateModel.ApplicationTitle.Text %>
+  </h1>
+
+  <div id="app"></div>
+  <script src="/assets/scripts/multi-ramp/legacy-api.js"></script>
+  <script src="/assets/scripts/multi-ramp/rv-main.js"></script>
+  <script
+    src="/assets/scripts/highcharts-editor/highcharts-editor.complete.js"
+  ></script>
+  <script src="/assets/js/chunk-vendors.a28bff94.js"></script>
+  <script src="/assets/js/chunk-common.b1125170.js"></script>
+  <script src="/assets/js/index.827e9960.js"></script>
+
+  
+</asp:Content>
---
+++ index.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GoC.WebTemplate.Components.Entities;
+using GoC.WebTemplate.WebForms;
+
+namespace Storylines
+{
+    public partial class base_page : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            WebTemplateMaster.WebTemplateModel.HTMLHeaderElements.Add("<script src=\"https://code.highcharts.com/highcharts.js\" charset=\"utf-8\"></script><script src=\"https://code.highcharts.com/modules/data.js\" charset=\"utf-8\"></script><link rel=\"icon\" href=\"favicon.svg\" /><link rel=\"stylesheet\" href=\"assets/scripts/multi-ramp/rv-styles.css\" /><link rel=\"stylesheet\" href=\"assets/scripts/highcharts-editor/highcharts-editor.min.css\" /><link href=\"assets/css/chunk-common.5f115248.css\" rel=\"preload\" as=\"style\" />\r\n    <link href=\"assets/css/chunk-vendors.5ce752f9.css\" rel=\"preload\" as=\"style\" />\r\n    <link href=\"assets/js/chunk-common.b1125170.js\" rel=\"preload\" as=\"script\" />\r\n    <link href=\"assets/js/chunk-vendors.a28bff94.js\" rel=\"preload\" as=\"script\" />\r\n    <link href=\"assets/js/index.827e9960.js\" rel=\"preload\" as=\"script\" />\r\n    <link href=\"assets/css/chunk-vendors.5ce752f9.css\" rel=\"stylesheet\" />\r\n    <link href=\"assets/css/chunk-common.5f115248.css\" rel=\"stylesheet\" />");
+            WebTemplateMaster.WebTemplateModel.DateModified = Convert.ToDateTime("2023-09-28", System.Globalization.CultureInfo.CurrentCulture);
+
+            //Page Title and Breadcrumbs
+            if (WebTemplateMaster.WebTemplateModel.TwoLetterCultureLanguage == "en") {
+                WebTemplateMaster.WebTemplateModel.HeaderTitle = "Storylines";
+                WebTemplateMaster.WebTemplateModel.ApplicationTitle.Text = "Storylines";
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://www.canada.ca/en.html", Title = "GoC" });
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://www.canada.ca/en/services/environment.html", Title = "Environment and natural resources" });
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://www.canada.ca/en/services/environment/cumulative-effect/federal-initiative.html", Title = "Storylines", });
+            } else {
+                WebTemplateMaster.WebTemplateModel.HeaderTitle = "[FR] Storylines";
+                WebTemplateMaster.WebTemplateModel.ApplicationTitle.Text = "[FR] Storylines";
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://www.canada.ca/fr.html", Title = "GoC", Acronym = "Gouvernement du Canada" });
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://www.canada.ca/fr/services/environnement.html", Title = "Environnement et ressources naturelles", Acronym = "Environnement et ressources naturelles" });
+                WebTemplateMaster.WebTemplateModel.Breadcrumbs.Add(new Breadcrumb { Href = "https://canada.ca/fr/services/environnement/effets-cumulatif/initiative-federale.html", Title = "Effets cumulatifs", Acronym = "[FR]Storylines" });
+            }
+        }
+    }
+}
