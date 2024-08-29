cbf7b3359ffb4544aeb7f324f823f08bb36cff01
mohsin-r <mohsin.reza@mail.utoronto.ca>
2024-08-29 14:18:18 -0400

    Externalize RAMP in config editor

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import router from './router';
 import './style.css';
 import { i18n } from './lang';
 
-import 'ramp-pcar/dist/lib/ramp.css';
 import VueMarkdownEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
