ee34e9554aaa5431f5fc0d7145cf75bcf4e97198
Ryan Coulson <ryan.coulson@ec.gc.ca>
2024-09-27 11:11:55 -0400

    correctly mark products with no logo as loaded

--- src/components/metadata-editor.vue
+++ src/components/metadata-editor.vue
@@ -945,6 +945,9 @@ export default class MetadataEditorV extends Vue {
                     }
                 });
             }
+        } else {
+            // If there's no logo, mark the product as loaded.
+            this.loadStatus = 'loaded';
         }
     }
 
