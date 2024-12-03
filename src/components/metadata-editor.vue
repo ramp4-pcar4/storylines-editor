4eedb7e78039e9f348c378a96439672eff797930
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2024-12-03 16:39:55 -0500

    Fix metadata editor toasts

--- src/components/metadata-editor.vue
+++ src/components/metadata-editor.vue
@@ -911,6 +911,12 @@ export default class MetadataEditorV extends Vue {
      */
     loadVersion(version: string): Promise<void> {
         return new Promise((resolve, reject) => {
+            if (this.uuid === undefined || this.uuid === '') {
+                Message.error(this.$t('editor.warning.mustEnterUuid'));
+                this.loadStatus = 'waiting';
+                reject();
+                return;
+            }
             // Create a new AbortController for each fetch attempt
             // as they get 'used up' after each successful abort() call
             this.controller = new AbortController();
@@ -925,10 +931,9 @@ export default class MetadataEditorV extends Vue {
                     if (res.status === 404) {
                         // Version not found.
                         if (version === 'latest') {
-                            Message.error(this.$t('editor.warning.uuidNotFound', this.uuid));
+                            Message.error(this.$t('editor.warning.uuidNotFound', { uuid: this.uuid }));
                         } else {
                             Message.error(this.$t('editor.editMetadata.message.error.noRequestedVersion'));
-                            this.loadStatus = 'loaded';
                         }
                         this.error = true;
                         this.loadStatus = 'waiting';
@@ -994,19 +999,21 @@ export default class MetadataEditorV extends Vue {
         // Note: This part probably doesn't need an manual abort() trigger to kill on load cancel,
         // as the history should be much smaller and quicker to fetch than the config
 
-        if (this.uuid === undefined) Message.error(this.$t('editor.warning.mustEnterUuid'));
+        if (this.uuid === undefined || this.uuid === '') {
+            return;
+        }
         this.loadStatus = 'loading';
         const user = useUserStore().userProfile.userName || 'Guest';
         fetch(this.apiUrl + `/history/${this.uuid}`, { headers: { user } }).then((res: Response) => {
             if (res.status === 404) {
                 // Product not found.
-                Message.error(`The requested UUID '${this.uuid ?? ''}' does not exist.`);
+                this.loadStatus = 'waiting';
             } else {
                 res.json().then((json) => {
                     this.storylineHistory = json;
+                    this.loadStatus = 'loaded';
                 });
             }
-            this.loadStatus = 'loaded';
         });
     }
 
