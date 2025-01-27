59df37aaa527abeb50ea4fa960ca4dcaee26ef38
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2025-01-27 14:15:31 -0500

    Improve next/prev slide buttons, modal designs

--- src/components/helpers/multi-option-modal.vue
+++ src/components/helpers/multi-option-modal.vue
@@ -1,29 +1,41 @@
 <template>
-    <vue-final-modal
-        :modalId="name"
-        :clickToClose="false"
-        content-class="flex flex-col max-h-full overflow-y-auto max-w-xl mx-4 p-4 bg-white border rounded-lg space-y-2"
-        class="flex justify-center items-center"
-    >
-        <div class="mx-5 my-2">
-            <h2 v-if="title" slot="header" class="text-2xl font-bold mb-1">{{ title }}</h2>
-            <p v-if="message">{{ message }}</p>
-            <div class="w-full flex justify-end mt-3">
-                <button
-                    v-for="option in options"
-                    class="editor-button bg-black text-white hover:bg-gray-800"
-                    @click="
-                        () => {
-                            option.action();
-                            this.$vfm.close(this.name);
-                        }
-                    "
+    <vue-final-modal :modalId="name" :clickToClose="false" content-class="" class="flex justify-center items-center">
+        <div
+            class="multi-option-modal flex flex-col max-h-full overflow-y-auto mx-4 p-4 bg-white border rounded-xl space-y-2"
+        >
+            <div class="mx-5 my-2">
+                <h2
+                    v-if="title"
+                    slot="header"
+                    class="text-2xl mb-3 text-pretty"
+                    style="font-weight: 700; line-height: 1.3"
                 >
-                    {{ option.label }}
-                </button>
-                <button v-if="cancelAllowed" class="editor-button hover:bg-gray-800" @click="onCancel">
-                    {{ $t('editor.cancel') }}
-                </button>
+                    {{ title }}
+                </h2>
+                <p v-if="message" style="font-weight: 500" class="text-pretty">{{ message }}</p>
+                <div class="w-full flex items-center gap-3 justify-end mt-5">
+                    <button
+                        v-for="option in options"
+                        class="editor-button bg-black text-white hover:bg-gray-800"
+                        style="margin: 0 !important"
+                        @click="
+                            () => {
+                                option.action();
+                                this.$vfm.close(this.name);
+                            }
+                        "
+                    >
+                        {{ option.label }}
+                    </button>
+                    <button
+                        v-if="cancelAllowed"
+                        class="editor-button hover:bg-gray-800"
+                        style="margin: 0 !important"
+                        @click="onCancel"
+                    >
+                        {{ $t('editor.cancel') }}
+                    </button>
+                </div>
             </div>
         </div>
     </vue-final-modal>
@@ -59,6 +71,12 @@ export default class MetadataEditorV extends Vue {
 </script>
 
 <style scoped lang="css">
+.multi-option-modal {
+    width: 500px !important;
+    min-width: 1px !important;
+    max-width: 90%;
+    border-radius: 0.75rem /* 12px */;
+}
 h2 {
     line-height: 1.3;
     border-bottom: 0px;
