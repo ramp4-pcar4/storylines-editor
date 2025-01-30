9dde2c8a60e49bd07ae57dd70abb96337f6dd6bd
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2025-01-30 17:32:32 -0500

    Implement robust, lightweight state tracking

--- src/components/metadata-editor.vue
+++ src/components/metadata-editor.vue
@@ -572,6 +572,7 @@
 
 <script lang="ts">
 import ActionModal from '@/components/helpers/action-modal.vue';
+import { Save, useStateStore } from '@/stores/stateStore';
 import { Options, Prop, Vue, Watch } from 'vue-property-decorator';
 import { RouteLocationNormalized } from 'vue-router';
 import { AxiosResponse } from 'axios';
@@ -669,6 +670,7 @@ export default class MetadataEditorV extends Vue {
     showDropdown = false;
     highlightedIndex = -1;
     lockStore = useLockStore();
+    stateStore = useStateStore();
     latestSchemaVersion = '';
 
     storylineHistory: History[] = [];
@@ -677,7 +679,20 @@ export default class MetadataEditorV extends Vue {
 
     // Saving properties.
     saving = false;
-    unsavedChanges = false;
+    unsavedChanges = this.stateStore.isChanged;
+
+    @Watch('stateStore.isChanged')
+    onChanged() {
+        this.unsavedChanges = this.stateStore.isChanged;
+    }
+
+    @Watch('stateStore.reconcileToggler')
+    onReconciliationRequest() {
+        const newConfigs = this.stateStore.addChangesToNewSave(this.stateStore.getCurrentChangeLocation());
+
+        this.configs.en = newConfigs.en;
+        this.configs.fr = newConfigs.fr;
+    }
 
     controller = new AbortController();
 
@@ -755,6 +770,11 @@ export default class MetadataEditorV extends Vue {
     sessionExpired: boolean = false;
     totalTime = import.meta.env.VITE_APP_CURR_ENV ? Number(import.meta.env.VITE_SESSION_END) : 30;
     productLoading = false;
+
+    // Debounce timer used for updateSaveStatus only.
+    // IMPORTANT: Avoid using stateStore's handlePotentialChange() directly, this timer may cause issues with change detection and saving to the configs variable.
+    // Instead, ALWAYS call either updateSaveStatus() (if in metadata-editor) or emit the 'save-status' event instead!
+    debounceTimer: ReturnType<typeof setTimeout> | null = null;
 
     @Watch('loadEditor')
     @Watch('loadStatus')
@@ -1895,31 +1915,53 @@ export default class MetadataEditorV extends Vue {
             return;
         }
 
+        // Fix various problems inside the config before load
+        // Add more as they're discovered
+        const fixConfigProblems = (config: StoryRampConfig) => {
+            config.slides.forEach((slide) => {
+                // If the slide has an undefined value for includeInToc, it's treated as true (as the schema wishes)
+                // This change prevents state management detection issues (undefined != true when compared)
+                slide.includeInToc = slide.includeInToc === undefined ? true : slide.includeInToc;
+            });
+
+            return config;
+        };
+
         try {
+            const stateSave = { en: {}, fr: {} };
+
             const enFile = this.configFileStructure?.zip.file(`${this.uuid}_en.json`);
             const frFile = this.configFileStructure?.zip.file(`${this.uuid}_fr.json`);
             await enFile?.async('string').then((res: string) => {
-                this.configs['en'] = JSON.parse(res);
+                const config = fixConfigProblems(JSON.parse(res));
+
+                this.configs['en'] = config;
+                stateSave.en = JSON.parse(JSON.stringify(config));
 
                 // Check if config contains a link to the default stylesheet, and if so add it in (only if it doesn't
                 // already contain it)
-                if (!this.configs['en'].stylesheets) {
-                    this.configs['en'].stylesheets = [`${this.uuid}/styles/main.css`];
-                } else if (!this.configs['en'].stylesheets.includes(`${this.uuid}/styles/main.css`)) {
-                    this.configs['en'].stylesheets.push([`${this.uuid}/styles/main.css`]);
-                }
+                // TODO: Fix (current implementation breaks state management)
+                // if (!this.configs['en'].stylesheets) {
+                //     this.configs['en'].stylesheets = [`${this.uuid}/styles/main.css`];
+                // } else if (!this.configs['en'].stylesheets.includes(`${this.uuid}/styles/main.css`)) {
+                //     this.configs['en'].stylesheets.push([`${this.uuid}/styles/main.css`]);
+                // }
             });
             await frFile?.async('string').then((res: string) => {
-                this.configs['fr'] = JSON.parse(res);
+                const config = fixConfigProblems(JSON.parse(res));
+                this.configs['fr'] = config;
+                stateSave.fr = JSON.parse(JSON.stringify(this.configs['fr']));
 
                 // Check if config contains a link to the default stylesheet, and if so add it in (only if it doesn't
                 // already contain it)
-                if (!this.configs['fr'].stylesheets) {
-                    this.configs['fr'].stylesheets = [`${this.uuid}/styles/main.css`];
-                } else if (!this.configs['fr'].stylesheets.includes(`${this.uuid}/styles/main.css`)) {
-                    this.configs['fr'].stylesheets.push([`${this.uuid}/styles/main.css`]);
-                }
+                // TODO: Fix (current implementation breaks state management)
+                // if (!this.configs['fr'].stylesheets) {
+                //     this.configs['fr'].stylesheets = [`${this.uuid}/styles/main.css`];
+                // } else if (!this.configs['fr'].stylesheets.includes(`${this.uuid}/styles/main.css`)) {
+                //     this.configs['fr'].stylesheets.push([`${this.uuid}/styles/main.css`]);
+                // }
             });
+            this.stateStore.save(stateSave as Save);
         } catch {
             Message.error(this.$t('editor.editMetadata.message.error.malformedProduct', this.uuid ?? ''));
             this.loadStatus = 'waiting';
@@ -1938,10 +1980,11 @@ export default class MetadataEditorV extends Vue {
         if (this.configs[this.configLang]) {
             this.useConfig(this.configs[this.configLang] as StoryRampConfig);
             this.findSources(this.configs); // increments source counts for all panels
+
             // Update router path
             if (this.reloadExisting) {
                 this.loadEditor = true;
-                this.unsavedChanges = false;
+                this.stateStore.isChanged = false;
                 this.updateEditorPath();
             } else if (!this.loadExisting) {
                 this.loadEditor = true;
@@ -2052,13 +2095,22 @@ export default class MetadataEditorV extends Vue {
         const frFileName = `${this.uuid}_fr.json`;
 
         // Replace undefined slides with empty objects
-        this.configs.en.slides = this.configs.en?.slides.map((slide) => {
-            return slide ?? {};
-        });
-        this.configs.fr.slides = this.configs.fr?.slides.map((slide) => {
-            return slide ?? {};
-        });
+        if (this.configs.en) {
+            this.configs.en.slides = this.configs.en.slides.map((slide) => {
+                return slide ?? {};
+            });
+        }
+
+        if (this.configs.fr) {
+            this.configs.fr.slides = this.configs.fr.slides.map((slide) => {
+                return slide ?? {};
+            });
+        }
+
         this.loadSlides(this.configs);
+
+        // TODO: Should we make stateStore save on every generateConfig activation, or just on pubish = true??
+        this.stateStore.save({ en: this.configs.en, fr: this.configs.fr });
 
         const engFormattedConfigFile = JSON.stringify(this.configs.en, null, 4);
         const frFormattedConfigFile = JSON.stringify(this.configs.fr, null, 4);
@@ -2068,7 +2120,6 @@ export default class MetadataEditorV extends Vue {
 
         // Upload the ZIP file.
         if (publish) {
-            this.unsavedChanges = false;
             this.configFileStructure?.zip.generateAsync({ type: 'blob' }).then((content: Blob) => {
                 const formData = new FormData();
                 formData.append('data', content, `${this.uuid}.zip`);
@@ -2195,12 +2246,18 @@ export default class MetadataEditorV extends Vue {
                 this.extendSession(true);
                 this.lockStore.broadcast?.postMessage({ action: 'saved' });
             }
+
+            // This may be necessary to allow for changes to be properly detected after saving.
+            // Without it, even attempted changes (e.g. keypress in an input field) are no longer detected, and the potential change handlers
+            // aren't even run.
+            // There's likely a better solution, please add if you find it.
+            this.loadConfig();
         }, 500);
     }
 
     updateMetadata<K extends keyof MetadataContent>(key: K, value: MetadataContent[K]): void {
         this.metadata[key] = value;
-        this.unsavedChanges = true;
+        this.updateSaveStatus(true, 'Update metadata');
     }
 
     discardMetadataUpdates(): void {
@@ -2263,6 +2320,8 @@ export default class MetadataEditorV extends Vue {
 
             const userStore = useUserStore();
             userStore.fetchUserProfile();
+
+            this.updateSaveStatus(true, 'Save metadata');
         }
         this.$vfm.close('metadata-edit-modal');
     }
@@ -2521,9 +2580,17 @@ export default class MetadataEditorV extends Vue {
 
     /**
      * Update the unsaved changes value to the payload.
+     * Debounced to prevent being called too often.
      */
-    updateSaveStatus(payload: boolean): void {
-        this.unsavedChanges = this.saving ? false : payload;
+    updateSaveStatus(payload: boolean, origin?: string): void {
+        if (this.debounceTimer) clearTimeout(this.debounceTimer);
+
+        this.debounceTimer = setTimeout(() => {
+            this.stateStore.handlePotentialChange(
+                { en: JSON.parse(JSON.stringify(this.configs.en)), fr: JSON.parse(JSON.stringify(this.configs.fr)) },
+                origin
+            );
+        }, 300);
     }
 
     refreshConfig(): void {
