b51d81ec920eb437a765c239795f32c04b023edf
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2025-03-04 15:33:20 -0500

    Improve slideshow editor

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -290,7 +290,7 @@
                                             />
                                         </g>
                                     </svg>
-                                    <p>{{ $t('editor.preview') }}</p>
+                                    <p class="truncate">{{ $t('editor.preview') }}</p>
                                     <svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         xmlns:xlink="http://www.w3.org/1999/xlink"
