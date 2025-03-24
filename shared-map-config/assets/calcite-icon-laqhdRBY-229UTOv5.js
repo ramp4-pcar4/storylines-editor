1871c4ad0c57a854ba0bb313a4b0d3b2e8986806
Yi Lei Feng <fengyilei1@gmail.com>
2025-03-24 13:52:17 +0000

    Delete PR demo from GH pages shared-map-config

--- shared-map-config/assets/calcite-icon-laqhdRBY-229UTOv5.js
+++
@@ -1,1 +0,0 @@
-import{f as t,m as o}from"./icon-BXFbBbop-CmWFRPLJ.js";import"./themeUtils-YjM7iIiX-DbhT_cxy.js";import"./main-Ccj-gfTQ.js";import"./uuid-Dj9mdEVg-BaKSCiyT.js";import"./dom-N58V0tYi-DBU3jlah.js";import"./observers-BigwqTsw-BYXPaKnr.js";const n=t,a=o;export{n as CalciteIcon,a as defineCustomElement};
--- shared-map-config/assets/compilerUtils-CV1QYWI8-9h4V49Ft.js
+++
@@ -1,1 +0,0 @@
-import"./main-Ccj-gfTQ.js";function o(n){}function r(n){return()=>n}export{r as o,o as t};
