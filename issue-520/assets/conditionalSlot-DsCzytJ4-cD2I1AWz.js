4cff102fed7c01f372c4a4059a775a3252f24f17
Yi Lei Feng <fengyilei1@gmail.com>
2025-03-26 18:33:20 +0000

    Delete PR demo from GH pages issue-520

--- issue-520/assets/conditionalSlot-DsCzytJ4-cD2I1AWz.js
+++
@@ -1,1 +0,0 @@
-import{n as i}from"./themeUtils-YjM7iIiX-t6ndXFr0.js";import{c}from"./observers-BigwqTsw-CkHKlJwx.js";let t;const e={childList:!0};function a(o){t||(t=c("mutation",r)),t.observe(o.el,e)}function m(o){t.unobserve(o.el)}function r(o){o.forEach(({target:n})=>{i(n)})}export{m as c,a as s};
