ae3793d4b88cdd912ba65f73fb08fc6249e6b3d2
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2025-04-08 13:13:39 -0400

    Improve text editor: Design, accessibility, functionality

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,37 @@ import './style.css';
 import { i18n } from './lang';
 
 import 'ramp-pcar/dist/ramp.css';
-import VueMarkdownEditor from '@kangc/v-md-editor';
+import VueMarkdownEditor from '@kangc/v-md-editor/lib/codemirror-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
+import '@kangc/v-md-editor/lib/style/codemirror-editor.css';
 import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
 import '@kangc/v-md-editor/lib/theme/style/github.css';
 import enUS from '@kangc/v-md-editor/lib/lang/en-US';
 import hljs from 'highlight.js';
 
+// Resources for the codemirror editor
+import Codemirror from 'codemirror';
+// mode
+import 'codemirror/mode/markdown/markdown';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+import 'codemirror/mode/vue/vue';
+// edit
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/matchbrackets';
+// placeholder
+import 'codemirror/addon/display/placeholder';
+// active-line
+import 'codemirror/addon/selection/active-line';
+// scrollbar
+import 'codemirror/addon/scroll/simplescrollbars';
+import 'codemirror/addon/scroll/simplescrollbars.css';
+// style
+import 'codemirror/lib/codemirror.css';
+
+VueMarkdownEditor.Codemirror = Codemirror;
 VueMarkdownEditor.lang.use('en-US', enUS);
 VueMarkdownEditor.use(githubTheme, {
     Hljs: hljs
