2c957055c34a28c70b8329ee72000062f6b0cc61
gordlin <gregwlg04@gmail.com>
2025-05-01 18:17:34 -0400

    Make minor improvements throughout editor

--- src/components/helpers/metadata-content.vue
+++ src/components/helpers/metadata-content.vue
@@ -121,10 +121,17 @@
                     />
                     <!-- Upload button -->
                     <button
+                        style="padding-top: 6.5px; padding-bottom: 6.5px"
                         @click.stop="openFileSelector('logoUpload')"
-                        class="editor-button mb-0.5 bg-black border border-black text-white hover:bg-gray-800"
+                        class="editor-button editor-forms-button mb-0.5 bg-black border border-black text-white hover:bg-gray-800 flex items-center gap-2 shadow-sm"
                     >
-                        {{ $t('editor.browse') }}
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                            <path
+                                fill="currentColor"
+                                d="M19 4H5a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h3c.55 0 1-.45 1-1s-.45-1-1-1H5V8h14v10h-3c-.55 0-1 .45-1 1s.45 1 1 1h3c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2m-7.35 6.35l-2.79 2.79c-.32.32-.1.86.35.86H11v5c0 .55.45 1 1 1s1-.45 1-1v-5h1.79c.45 0 .67-.54.35-.85l-2.79-2.79c-.19-.2-.51-.2-.7-.01"
+                            />
+                        </svg>
+                        <div>{{ $t('editor.browse') }}</div>
                     </button>
                     <!-- Delete button -->
                     <button
@@ -246,10 +253,17 @@
                         />
                         <!-- Upload button -->
                         <button
+                            style="padding-top: 6.5px; padding-bottom: 6.5px"
                             @click.stop="openFileSelector('backgroundUpload')"
-                            class="editor-button mb-0.5 bg-black border border-black text-white hover:bg-gray-800"
+                            class="editor-button editor-forms-button mb-0.5 bg-black border border-black text-white hover:bg-gray-800 flex items-center gap-2 shadow-sm"
                         >
-                            {{ $t('editor.browse') }}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path
+                                    fill="currentColor"
+                                    d="M19 4H5a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h3c.55 0 1-.45 1-1s-.45-1-1-1H5V8h14v10h-3c-.55 0-1 .45-1 1s.45 1 1 1h3c1.1 0 2-.9 2-2V6a2 2 0 0 0-2-2m-7.35 6.35l-2.79 2.79c-.32.32-.1.86.35.86H11v5c0 .55.45 1 1 1s1-.45 1-1v-5h1.79c.45 0 .67-.54.35-.85l-2.79-2.79c-.19-.2-.51-.2-.7-.01"
+                                />
+                            </svg>
+                            <div>{{ $t('editor.browse') }}</div>
                         </button>
                         <!-- Delete button -->
                         <button
