9c9a75c48a18607134e108cc0154b039b5199e14
Gordon Lin <75815453+gordlin@users.noreply.github.com>
2025-05-02 15:43:29 -0400

    Fix even more app-wide nitpicks

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        <div class="flex justify-between items-center p-8 border-b border-solid border-black">
-            <h1 class="text-2xl md:text-3xl font-semibold mt-4">{{ $t('editor.respectTitle') }}</h1>
+        <!-- Header bar -->
+        <header class="flex justify-between flex-wrap items-center pt-3 pb-2 border-b-2 border-solid border-black mb-5">
+            <div class="flex items-center gap-3 px-7 md:px-20">
+                <!-- Icon -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 36 36">
+                    <path
+                        fill="#DD2E44"
+                        d="M36 20.917c0-.688-2.895-.5-3.125-1s3.208-4.584 2.708-5.5s-5.086 1.167-5.375.708c-.288-.458.292-3.5-.208-3.875s-5.25 4.916-5.917 4.292s1.542-10.5 1.086-10.698s-3.419 1.365-3.793 1.282C21.002 6.042 18.682 0 18 0s-3.002 6.042-3.376 6.125s-3.337-1.48-3.793-1.282s1.752 10.073 1.085 10.698C11.25 16.166 6.5 10.875 6 11.25s.08 3.417-.208 3.875c-.289.458-4.875-1.625-5.375-.708s2.939 5 2.708 5.5s-3.125.312-3.125 1s8.438 5.235 9 5.771s-2.914 2.802-2.417 3.229c.576.496 3.839-.83 10.417-.957V35a1 1 0 1 0 2 0v-6.04c6.577.127 9.841 1.453 10.417.957c.496-.428-2.979-2.694-2.417-3.229s9-5.084 9-5.771"
+                    />
+                </svg>
+                <!-- Application title -->
+                <h1 style="padding-top: 2px" class="text-xl font-semibold">
+                    {{ $t('editor.respectTitle') }}
+                </h1>
+            </div>
+            <!-- Language toggle -->
             <router-link
+                class="px-7 md:px-20"
                 :to="{ name: 'home', params: { lang: currLang === 'en' ? 'fr' : 'en' } }"
                 v-if="!sourceFile.includes('index-ca')"
             >
                 <div class="underline">{{ `${currLang === 'en' ? 'Français' : 'English'}` }}</div>
             </router-link>
-        </div>
-        <div class="relative px-7 md:px-32">
+        </header>
+        <div class="relative" style="margin-right: 10%; margin-left: 10%">
             <div v-if="showExpired" class="w-full rounded-md bg-red-100 p-2 mt-5">
                 <span class="flex flex-row items-center">
                     <svg
