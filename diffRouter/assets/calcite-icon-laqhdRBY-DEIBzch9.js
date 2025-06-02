755f920770920840e4d7d107c84799b289f96e63
Yi Lei Feng <fengyilei1@gmail.com>
2025-06-02 13:40:43 +0000

    Delete PR demo from GH pages diffRouter

--- diffRouter/assets/calcite-icon-laqhdRBY-DEIBzch9.js
+++
@@ -1,1 +0,0 @@
-import{f as t,m as o}from"./icon-BXFbBbop-COhu6nRt.js";import"./themeUtils-YjM7iIiX-C_yHJqEB.js";import"./main-rAT-DIbt.js";import"./uuid-Dj9mdEVg-BaKSCiyT.js";import"./dom-N58V0tYi-DBU3jlah.js";import"./observers-BigwqTsw-CVky_Cam.js";const n=t,a=o;export{n as CalciteIcon,a as defineCustomElement};
