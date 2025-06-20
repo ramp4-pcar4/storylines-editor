f79795411b9fb701ca1ea6097c11a3e702a89876
Yi Lei Feng <fengyilei1@gmail.com>
2025-06-20 15:37:21 +0000

    Delete PR demo from GH pages header-overlap

--- header-overlap/assets/calcite-icon-laqhdRBY-B2ugvnVQ.js
+++
@@ -1,1 +0,0 @@
-import{f as t,m as o}from"./icon-BXFbBbop-BUG80mQK.js";import"./themeUtils-YjM7iIiX-DmeOQi2t.js";import"./main-Dxn-tpqk.js";import"./uuid-Dj9mdEVg-BaKSCiyT.js";import"./dom-N58V0tYi-DBU3jlah.js";import"./observers-BigwqTsw-BNxiqsYD.js";const n=t,a=o;export{n as CalciteIcon,a as defineCustomElement};
--- header-overlap/assets/vue.esm-bundler-D2erd_gM-CEhosukQ.js
+++
@@ -1,1 +0,0 @@
-import{fA as o,gw as t}from"./main-Dxn-tpqk.js";const r=o(t);export{r as o};
