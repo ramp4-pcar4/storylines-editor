d8cf1d29b3f04d96a2fcb1a1af1d6872cb3b8523
Yi Lei Feng <fengyilei1@gmail.com>
2025-06-26 13:18:14 +0000

    Delete PR demo from GH pages lowercase-uuid

--- lowercase-uuid/assets/portalUtils-CKN9iXwH-AUwUFImY.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./main-CRLG_6R7.js";function f(r,n){return r===null?n:new u({url:r.field("url")})}export{f as n};
--- lowercase-uuid/assets/vue.esm-bundler-D2erd_gM-M9druphf.js
+++
@@ -1,1 +0,0 @@
-import{fA as o,gw as t}from"./main-CRLG_6R7.js";const r=o(t);export{r as o};
