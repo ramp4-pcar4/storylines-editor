d13bf719168392a6c68ca098db30e9e1ba300a5b
Yi Lei Feng <fengyilei1@gmail.com>
2025-07-09 14:14:47 +0000

    Delete PR demo from GH pages integrate-HACK

--- integrate-HACK/assets/requestImageUtils-Ut4YcoNU-DE8Gze0a.js
+++
@@ -1,1 +0,0 @@
-import{f as r}from"./main-CWyaPalK.js";async function o(a,e){const{data:n}=await r(a,{responseType:"image",...e});return n}export{o as r};
--- integrate-HACK/assets/vue.esm-bundler-D2erd_gM-BvJSXPZA.js
+++
@@ -1,1 +0,0 @@
-import{fA as o,gw as t}from"./main-CWyaPalK.js";const r=o(t);export{r as o};
