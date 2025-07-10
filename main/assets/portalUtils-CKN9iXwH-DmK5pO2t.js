b76d6742585dd4b5b7b8448771baa5cd0c497b38
szczz <matt.szczerba55@gmail.com>
2025-07-10 14:29:21 +0000

    Deploying to gh-pages from @ ramp4-pcar4/storylines-editor@4a2173485219f6446ce71e604ea60c9d4f3e1f10 🚀

--- main/assets/portalUtils-CKN9iXwH-DmK5pO2t.js
+++
@@ -1,1 +0,0 @@
-import{r as u}from"./main-CWyaPalK.js";function f(r,n){return r===null?n:new u({url:r.field("url")})}export{f as n};
--- main/assets/scaleUtils-DxsfQwK1-C4-neQM4.js
+++
@@ -1,1 +0,0 @@
-import{p as c,fk as f,fl as o}from"./main-CWyaPalK.js";const r=96;function p(n,t){const e=n.extent,u=n.width,i=c(e?.spatialReference);return e&&u?e.width/u*i*f*r:0}function m(n,t){return n/(c(t)*f*r)}function w(n,t,e){return a(n,t)&&s(n,e)}function a(n,t){return t===0||o(n,t)||n<t}function s(n,t){return t===0||o(n,t)||n>t}export{w as b,p as c,m as f};
