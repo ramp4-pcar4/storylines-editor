d770bf6b54579e631362a061e52d91f51c0b9caa
Yi Lei Feng <fengyilei1@gmail.com>
2025-07-10 14:31:34 +0000

    Delete PR demo from GH pages dynamicstyling

--- dynamicstyling/assets/calcite-icon-laqhdRBY-DJQZHJVK.js
+++
@@ -1,1 +0,0 @@
-import{f as t,m as o}from"./icon-BXFbBbop-D9-pLuO4.js";import"./themeUtils-YjM7iIiX-V-EuNDTt.js";import"./main-40xFs5mr.js";import"./uuid-Dj9mdEVg-BaKSCiyT.js";import"./dom-N58V0tYi-DBU3jlah.js";import"./observers-BigwqTsw-BnAG8gMZ.js";const n=t,a=o;export{n as CalciteIcon,a as defineCustomElement};
--- dynamicstyling/assets/compilerUtils-CV1QYWI8-DP-eayh2.js
+++
@@ -1,1 +0,0 @@
-import"./main-40xFs5mr.js";function o(n){}function r(n){return()=>n}export{r as o,o as t};
