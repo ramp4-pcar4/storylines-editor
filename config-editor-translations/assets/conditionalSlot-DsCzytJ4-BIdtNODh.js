57021c90931bf243d32d0ea94e65d81de7832a18
Yi Lei Feng <fengyilei1@gmail.com>
2025-07-16 16:09:15 +0000

    Delete PR demo from GH pages config-editor-translations

--- config-editor-translations/assets/conditionalSlot-DsCzytJ4-BIdtNODh.js
+++
@@ -1,1 +0,0 @@
-import{n as i}from"./themeUtils-YjM7iIiX-BqmXNdE-.js";import{c}from"./observers-BigwqTsw-CV8JQp3n.js";let t;const e={childList:!0};function a(o){t||(t=c("mutation",r)),t.observe(o.el,e)}function m(o){t.unobserve(o.el)}function r(o){o.forEach(({target:n})=>{i(n)})}export{m as c,a as s};
