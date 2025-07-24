a51c6e2ccc632c3cf878f378b216fa06c5c356c0
Yi Lei Feng <fengyilei1@gmail.com>
2025-07-24 19:51:49 +0000

    Delete PR demo from GH pages remove-console-logs

--- remove-console-logs/assets/useT9n-ao51JRwP-DD6AmEMm.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./main-Ct-UVf4e.js";import{u as r}from"./jsxFactory-C5LxVioS-BABdTHZX.js";const s=o(r);export{s};
--- remove-console-logs/assets/vue.esm-bundler-EeyxqZGJ-Degc8Ugm.js
+++
@@ -1,1 +0,0 @@
-import{gQ as o,hN as s}from"./main-Ct-UVf4e.js";const r=o(s);export{r as s};
