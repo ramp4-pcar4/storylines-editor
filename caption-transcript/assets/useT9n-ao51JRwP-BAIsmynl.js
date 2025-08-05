95941be9e808b6de6776b1bc8e176c807c53d5ba
Yi Lei Feng <fengyilei1@gmail.com>
2025-08-05 14:53:22 +0000

    Delete PR demo from GH pages caption-transcript

--- caption-transcript/assets/useT9n-ao51JRwP-BAIsmynl.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./main-4y9R8zVP.js";import{u as r}from"./jsxFactory-C5LxVioS-DCoVpi3y.js";const s=o(r);export{s};
--- caption-transcript/assets/vue.esm-bundler-EeyxqZGJ-33Mf09l6.js
+++
@@ -1,1 +0,0 @@
-import{gQ as o,hN as s}from"./main-4y9R8zVP.js";const r=o(s);export{r as s};
