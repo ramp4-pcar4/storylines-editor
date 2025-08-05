72f1999a23e86f8320ade928d8027e06e63b4980
Yi Lei Feng <fengyilei1@gmail.com>
2025-08-05 14:43:47 +0000

    Delete PR demo from GH pages dynamic-editor-refresh

--- dynamic-editor-refresh/assets/executeForIds-MucOSmLz-BHwBNTn6.js
+++
@@ -1,1 +0,0 @@
-import{y as c}from"./utils-DuaeuwP5-DR8QuO6z.js";import{w as m,N as f}from"./query-nJmB7Ppn-oQ9khfsl.js";import{b as i}from"./Query-CxQYWcUQ-Cpf7_uO2.js";async function p(t,o,r){const n=c(t),{data:a}=await m(n,i.from(o),r);return a.count}async function d(t,o,r){const n=c(t),{data:a}=await f(n,i.from(o),r);return a.objectIds??[]}export{d as e,p as i};
--- dynamic-editor-refresh/assets/useT9n-ao51JRwP-CWWXj-gM.js
+++
@@ -1,1 +0,0 @@
-import{i7 as o}from"./main-DUOxjmwN.js";import{u as r}from"./jsxFactory-C5LxVioS-CjO32UVe.js";const s=o(r);export{s};
