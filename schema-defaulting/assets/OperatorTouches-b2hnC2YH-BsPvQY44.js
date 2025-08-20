dd36d1ddc4702d7317f581b560e12de5352d60fb
Yi Lei Feng <fengyilei1@gmail.com>
2025-08-20 14:01:10 +0000

    Delete PR demo from GH pages schema-defaulting

--- schema-defaulting/assets/OperatorTouches-b2hnC2YH-BsPvQY44.js
+++
@@ -1,1 +0,0 @@
-import{B as n,V as s}from"./ProjectionTransformation-BocI5WO--COz4QjK2.js";class u extends n{getOperatorType(){return 5}execute(e,r,t,a){return s(e,r,t,8,a)}}export{u as n};
--- schema-defaulting/assets/disjointOperator-DRt-x7F0-CcybAVwB.js
+++
@@ -1,1 +0,0 @@
-import{v as r}from"./ProjectionTransformation-BocI5WO--COz4QjK2.js";import{fromGeometry as m,fromGeometryToGXGeometry as a}from"./jsonConverter-DldrjI06-Bie5vAPy.js";import"./SimpleGeometryCursor-DSF_YyIm-TAMgMsbK.js";import"./main-CRFFCXJC.js";const f=new r;function p(t,o){const e=m(t);return f.execute(e.getGeometry(),a(o),e.getSpatialReference())}export{p as execute};
