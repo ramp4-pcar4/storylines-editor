008d1ec8c3e811250313f024b3b672ff7d44f429
Yi Lei Feng <fengyilei1@gmail.com>
2025-11-14 20:09:00 +0000

    Delete PR demo from GH pages enhance-default-chart-naming

--- enhance-default-chart-naming/assets/OperatorOverlaps-DVRlZOh6-CQhPQRcA.js
+++
@@ -1,1 +0,0 @@
-import{L as a,k as n}from"./ProjectionTransformation-DQ4GElxa-DpdVqJdj.js";class c extends a{getOperatorType(){return 9}execute(e,r,t,s){return n(e,r,t,32,s)}}export{c as n};
--- enhance-default-chart-naming/assets/vue.esm-bundler-OQ9kCVY2-BJTckwU2.js
+++
@@ -1,1 +0,0 @@
-import{g_ as o,hX as p}from"./main-CgXE8sFZ.js";const s=o(p);export{s as o};
